7ac536fc7cabe80187151b925f5390f49ae37209
tterminator <tterminator@sina.com>
2016-04-11 16:07:12 +0800

    int the main function, do the way of runnable with CountDownLatch

--- src/cas/CasTest.java
+++ src/cas/CasTest.java
@@ -1,5 +1,7 @@
 package cas;
 
+import java.util.concurrent.CountDownLatch;
+
 import counter.SafeCounter;
 import counter.UnsafeCounter;
 import runnablePackage.RunnableWithCAS;
@@ -33,22 +35,62 @@ public class CasTest {
 		waitSubThreadComplete();
 		System.out.println("doWithThread, safe result: " + safeCounter.getValue());
 	}
-
+	
+	/**
+	 * 使用CountDownLatch保证所有子进程完成后再输出结果
+	 * */
 	public static void doWithRunnable(){
 		//操作非线程安全计数器
+		final CountDownLatch unsafeEndGate = new CountDownLatch(10000);
 		for(int i = 1; i <= 10000; i++){
-			Thread t = new Thread(new RunnableWithoutCAS());
+			Thread t = new Thread(){
+				public void run(){
+					try{
+						try{
+							new RunnableWithoutCAS().run();
+						}finally{
+							unsafeEndGate.countDown();
+						}
+					}catch(Exception e){
+						
+					}
+				}
+			};
 			t.start();
 		}
-		waitSubThreadComplete();//使main线程等待它启动的所有子进程完成后，打印计数器结果
+//		waitSubThreadComplete();//使main线程等待它启动的所有子进程完成后，打印计数器结果
+		try {
+			unsafeEndGate.await();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
 		System.out.println("doWithRunnable,unsafe result: " + RunnableWithoutCAS.getUnsafeCounter().getValue());
 		
 		//操作线程安全计数器
+		final CountDownLatch safeEndGate = new CountDownLatch(10000);
 		for(int i = 1; i <= 10000; i++){
-			Thread t = new Thread(new RunnableWithCAS());
+			Thread t = new Thread(){
+				public void run(){
+					try{
+						try{
+							new RunnableWithCAS().run();
+						}finally{
+							safeEndGate.countDown();
+						}
+					}catch(Exception e){
+						
+					}
+				}
+			};
 			t.start();
 		}
-		waitSubThreadComplete();
+//		waitSubThreadComplete();
+		try {
+			safeEndGate.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("doWithRunnable,safe result:   " + RunnableWithCAS.getSafeCounter().getValue());
 	}
 	
